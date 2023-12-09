f71633c4e09bbe83a12a5a380d1a14ad01782fb2
dtr6210 <rialsdean@gmail.com>
2023-12-09 14:15:49 -0600

    completed lab6

---
+++ EXPRESS/myapp/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const testRoutes = require('./routes/myTestRoutes');
+const calculatorRoutes = require('./routes/calculatorRoutes')
+
+app.use('/testRoutes', testRoutes);
+
+app.use('/calculator', calculatorRoutes)
+
+
+app.use("/", express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.listen(3001, () => {
+  console.log(`Example app listening at http://localhost:3001`);
+});
